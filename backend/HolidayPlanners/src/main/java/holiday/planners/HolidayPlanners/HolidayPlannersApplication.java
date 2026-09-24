0bdd4a49be10992e7c9ca8e5cf8992b18094e3e8
Umwizer <ruthumwizerwa@gmail.com>
2026-09-24 13:14:53 +0200

    springboot initialization

---
+++ backend/HolidayPlanners/src/main/java/holiday/planners/HolidayPlanners/HolidayPlannersApplication.java
@@ -0,0 +1,13 @@
+package holiday.planners.HolidayPlanners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayPlannersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayPlannersApplication.class, args);
+	}
+
+}
---
+++ backend/HolidayPlanners/src/test/java/holiday/planners/HolidayPlanners/HolidayPlannersApplicationTests.java
@@ -0,0 +1,13 @@
+package holiday.planners.HolidayPlanners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayPlannersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
